2b17fd89482ef5d6c4539dc21c8180f24dab3b63
黄照样 <554598474@qq.com>
2024-12-19 18:16:59 +0800

    update requirement.txt

--- tests/method/test_fate_method.py
+++ tests/method/test_fate_method.py
@@ -4,7 +4,12 @@ import cfe
 
 class TestFateMethod():
 
+    def setup_method(self):
+        TestFateMethod
     def test_init(self):
         pass
     def test_choose_backend(self):
        pass
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
